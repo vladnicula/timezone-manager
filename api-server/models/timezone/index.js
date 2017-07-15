383e07544d0091c2f6f55a604fa020f4b5e8680e
Vlad Nicula <vladnicula@gmail.com>
2017-07-15 17:53:27 +0300

    Allow duplicate timezones by name as long as they have different cities

--- api-server/models/timezone/index.js
+++ api-server/models/timezone/index.js
@@ -27,23 +27,23 @@ const timezoneSchema = new mongoose.Schema({
 });
 
 // disallow inserting duplicate fields by any means
-timezoneSchema.index({ name: 1, userId: 1 }, { unique: true });
+timezoneSchema.index({ name: 1, city: 1, userId: 1 }, { unique: true });
 timezoneSchema.index({ name: 'text' });
 
 timezoneSchema.pre('save', function (next) {
-  const { _id, name, userId } = this;
-  mongoose.models.Timezone.findOne({ name, userId }, (err, results) => {
+  const { _id, name, city, userId } = this;
+  mongoose.models.Timezone.findOne({ name, city, userId }, (err, results) => {
     if (err) {
       return next(err);
     } else if (results) { // there was a result found, so this would be a dupliccate
       if (results._id.toString() === _id.toString()) {
         return next();
       }
-      this.invalidate('name', 'the name is already taken by another record owned by the same user');
+      this.invalidate('name', 'the name and city combination is already taken by another record');
       const error = new Error('Name UserId pair must be unique');
       error.name = 'ValidationError';
       error.errors = {
-        name: 'the name is already taken by another record owned by the same user',
+        name: 'the name and city combination is already taken by another record owned by the same user',
       };
       return next(error);
     }
